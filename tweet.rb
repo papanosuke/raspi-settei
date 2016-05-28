d4c16fa849dbf6ce61b7d832da07bc2e504db9e5
papanosuke <cbf17819@nifty.com>
2016-05-28 19:46:15 +0900

    word & photo tweet

--- tweet.rb
+++ tweet.rb
@@ -1,5 +1,6 @@
 require 'twitter'
 
+#カスタマーキー、アクセストークン指定
 client = Twitter::REST::Client.new do |config|
   config.consumer_key          =  "3RlO6WyYwiwHEEy2SlSAgwfPA"
   config.consumer_secret       =  "PhfISYxBtSl61SerV7iQ4fthszvpf5IzrDJC4tAYSFx7PZDmkf"
@@ -13,4 +14,21 @@ File.open("sentences.txt","r") do |l|
     s.push(line)
   end
 end
-client.update_with_media(s.sample,open("/home/pi/a.png"))
+
+#tmpディレクトリ指定
+dir = Dir.glob("/tmp_motion/**")
+
+#ツイート
+dir.each do |file|
+  client.update_with_media(s.sample,open(file))
+end
+
+#削除
+begin 
+  dir.each do |file|
+    File.unlink(file)
+  end
+rescue StandardError => ex
+ensure
+end
+
