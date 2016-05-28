48f47011399bc9005711673134df2932ba2f8124
papanosuke <cbf17819@nifty.com>
2016-05-28 13:24:55 +0900

    add new 3 files(tweet program)

---
+++ tweet.rb
@@ -0,0 +1,16 @@
+require 'twitter'
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key          =  "3RlO6WyYwiwHEEy2SlSAgwfPA"
+  config.consumer_secret       =  "PhfISYxBtSl61SerV7iQ4fthszvpf5IzrDJC4tAYSFx7PZDmkf"
+  config.access_token          =  "136484894-2qyEsTEiLMfmlRQmbSpqXuaszvuwuPSVr9Xc5H83"
+  config.access_token_secret   =  "GIoBkh5sYmsN72yUse6Z6GaebdV55IABJXoWRCanY0MdS"
+end
+
+s=[]
+File.open("sentences.txt","r") do |l|
+  l.each_line do |line|
+    s.push(line)
+  end
+end
+client.update(s.sample)
